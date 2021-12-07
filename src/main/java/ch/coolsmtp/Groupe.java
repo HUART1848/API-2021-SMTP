56aa0d3475a9d75f04dce8c042e21df5250e6841
alice <alice@nerdgautica.net>
2021-12-07 14:25:26 +0100

    Merge branch 'main' of D:\HEIG\Semestre3\API\labo\labo4\HUART-API-2021-SMTP with conflicts.

--- src/main/java/ch/coolsmtp/Groupe.java
+++ src/main/java/ch/coolsmtp/Groupe.java
@@ -1,5 +1,19 @@
 package ch.coolsmtp;
 
 public class Groupe {
-    
+
+    private String sender;
+    private String[] victimes;
+
+    public Groupe(String[] v){
+        sender = v[0];
+        victimes = new String[v.length - 1];
+        System.arraycopy(v, 1, victimes, 0 , v.length - 1);
+    }
+
+    public Groupe[] makeGroupes(String filePath, int nbrOfGroupes){
+        Groupe[] ret = new Groupe[nbrOfGroupes];
+        
+        return ret;
+    }
 }

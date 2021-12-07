04f5b0abc4c1e03b65a0d7f73cc8d73af1a61b0a
Hugo Huart <hugo.huart@heig-vd.ch>
2021-12-07 11:31:08 +0100

    ajoute des choses

---
+++ src/main/java/ch/coolsmtp/App.java
@@ -0,0 +1,14 @@
+package ch.coolsmtp;
+
+public class App {
+    public static void main(String[] args) {
+        Person p0 = new Person(0, "coucou@gmail.com");
+        Person p1 = new Person(1, "coucou@gmail.com");
+        Person p2 = new Person(2, "coucou@gmail.com");
+        Person p3 = new Person(3, "coucou@gmail.com");
+        Person p4 = new Person(4, "coucou@gmail.com");
+
+        Group g0 = new Group(0, p0, p1, p2, p3);
+        System.out.println(g0);
+    }
+}
---
+++ src/main/java/ch/coolsmtp/Group.java
@@ -0,0 +1,29 @@
+package ch.coolsmtp;
+
+import java.util.Arrays;
+
+public class Group {
+    private int no;
+
+    private Person sender;
+    private Person[] receiver;
+
+    public Group(int no, Person sender, Person... receiver) {
+        this.no = no;
+        this.sender = sender;
+        this.receiver = Arrays.copyOf(receiver, receiver.length);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("=== GROUP %d ===\n", no));
+        sb.append(String.format("SENDER\n\t%s\n", sender));
+
+        sb.append("RECEIVERS : \n");
+        for (Person p : receiver)
+            sb.append(String.format("\t%s\n", p));
+
+        return sb.toString();
+    }
+}

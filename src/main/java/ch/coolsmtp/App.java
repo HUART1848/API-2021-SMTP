8e8d39ceede03d89a259626eefa9cb5ca4996d6a
Hugo Huart <hugo.huart@heig-vd.ch>
2021-12-07 14:47:40 +0100

    Print empty group

--- src/main/java/ch/coolsmtp/App.java
+++ src/main/java/ch/coolsmtp/App.java
@@ -1,17 +1,24 @@
 package ch.coolsmtp;
 
+import ch.coolsmtp.mail.Group;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("=== coolsmtp : SMTP-based app to prank your friends ===");
 
         Config config = new Config();
-        System.out.println("Loading config (config.txt)...");
-        if (!config.parseConfigFromFile("src/main/config/config.txt")) {
+        System.out.println("Loading config...");
+
+        if (!config.parseConfigFromFile("config/config.txt")) {
             System.out.println("ERROR while attempting to load the config file.\n" +
                     "Please check for config file existence and/or fix config file syntax");
+            return;
         }
 
         System.out.println("Config loaded");
         System.out.println(config);
+
+        Group g = new Group(0);
+        System.out.println(g);
     }
 }
--- src/main/java/ch/coolsmtp/mail/Group.java
+++ src/main/java/ch/coolsmtp/mail/Group.java
@@ -8,16 +8,30 @@ public class Group {
     private Person sender;
     private Person[] receiver;
 
+    public Group(int no) {
+        this.no = no;
+    }
+
     public Group(int no, Person sender, Person... receiver) {
         this.no = no;
         this.sender = sender;
         this.receiver = Arrays.copyOf(receiver, receiver.length);
     }
 
+    public boolean parseGroupFromFile() {
+        return true;
+    }
+
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
         sb.append(String.format("=== GROUP %d ===\n", no));
+
+        if (sender == null || receiver == null) {
+            sb.append("Empty group...");
+            return sb.toString();
+        }
+
         sb.append(String.format("SENDER\n\t%s\n", sender));
 
         sb.append("RECEIVERS : \n");
